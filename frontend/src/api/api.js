a2e7b9a65a05150ee194e2fa6a09341ccaad5615
bharathgoud000 <pachimatlabharath143@gmail.com>
2026-05-09 21:45:55 +0530

    environ changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://team-task-manager-ww80.onrender.com",
+  baseURL: "import.meta.env.VITE_API_URL",
 });
 
 api.interceptors.request.use((config) => {
