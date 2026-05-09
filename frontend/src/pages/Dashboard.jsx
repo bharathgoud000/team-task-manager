2554b7c09c5d2cbbca05a529a512e727ab9e50ec
bharathgoud000 <pachimatlabharath143@gmail.com>
2026-05-09 22:02:06 +0530

    fixed issue

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import StatCard from "../components/StatCard";
+import StatCard from "../components/Statcard";
 import "../styles/Dashboard.css";
 
 export default function Dashboard() {
